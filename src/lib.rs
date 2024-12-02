1e8f5b9fe11040f0644e48e29f1c6257fabf1c63
enrico-car <car.enrico19@gmail.com>
2024-12-02 16:24:57 +0100

    logger funztion

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+mod logger;
+
+use logger::*;
---
+++ src/logger.rs
@@ -0,0 +1,43 @@
+
+pub struct Logger {
+    displayable: bool,
+    module: String,
+}
+
+
+impl Logger {
+    pub fn new(displayable: bool) -> Logger {
+        Logger {
+            displayable: displayable,
+            module: module_path!().to_string(),
+        }
+    }
+    
+    pub fn set_displayable(&mut self, displayable: bool) {
+        self.displayable = displayable;
+    }
+    
+    pub fn log_debug(&self, message: &str) {
+        if self.displayable {
+            println!("\x1b[32m[DEBUG][{}]-->{}\x1b[0m",self.module,  message);
+        }
+    }
+    
+    pub fn log_info(&self, message: &str) {
+        if self.displayable {
+            println!("\x1b[34m[INFO][{}]-->{}\x1b[0m",self.module,  message);
+        }
+    }
+    
+    pub fn log_warn(&self, message: &str) {
+        if self.displayable {
+            println!("\x1b[33m[WARN][{}]-->{}\x1b[0m",self.module,  message);
+        }
+    }
+    
+    pub fn log_error(&self, message: &str) {
+        if self.displayable {
+            println!("\x1b[31m[ERR][{}]-->{}\x1b[0m",self.module,  message);
+        }
+    }
+}
