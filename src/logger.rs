c465557aea5e2b546fbfcdb74466caf477b7afc3
enrico-car <car.enrico19@gmail.com>
2024-12-02 17:27:54 +0100

    minor

--- src/logger.rs
+++ src/logger.rs
@@ -19,25 +19,25 @@ impl Logger {
     
     pub fn log_debug(&self, message: &str) {
         if self.displayable {
-            println!("\x1b[32m[DEBUG][{}]-->{}\x1b[0m",self.module,  message);
+            println!("\x1b[32m[DEBUG][{}]    ||{}\x1b[0m",self.module,  message);
         }
     }
     
     pub fn log_info(&self, message: &str) {
         if self.displayable {
-            println!("\x1b[34m[INFO][{}]-->{}\x1b[0m",self.module,  message);
+            println!("\x1b[34m[INFO][{}]    ||{}\x1b[0m",self.module,  message);
         }
     }
     
     pub fn log_warn(&self, message: &str) {
         if self.displayable {
-            println!("\x1b[33m[WARN][{}]-->{}\x1b[0m",self.module,  message);
+            println!("\x1b[33m[WARN][{}]    ||{}\x1b[0m",self.module,  message);
         }
     }
     
     pub fn log_error(&self, message: &str) {
         if self.displayable {
-            println!("\x1b[31m[ERR][{}]-->{}\x1b[0m",self.module,  message);
+            println!("\x1b[31m[ERR][{}]     ||{}\x1b[0m",self.module,  message);
         }
     }
 }
