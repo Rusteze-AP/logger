39ec6435b60295e2bc60d6da6083135ab7452cf2
enrico-car <car.enrico19@gmail.com>
2024-12-08 17:52:45 +0100

    websocket

--- src/logger.rs
+++ src/logger.rs
@@ -1,59 +1,113 @@
 use chrono::Local;
+use futures::SinkExt;
+use futures::StreamExt;
+use std::thread;
+use tokio::runtime::Runtime;
+use tokio::sync::broadcast;
+use warp::Filter;
+
+#[derive(Debug, Clone, Copy)]
+pub enum LogLevel {
+    None = 0b0000, // 0
+    Debug = 0b0001, // 1
+    Warn  = 0b0010, // 2
+    Error = 0b0100, // 4
+    Info  = 0b1000, // 8
+    All = 0b1111, // 15
+}
 
 pub struct Logger {
-    displayable: bool,
+    displayable: u8,
     module: String,
+    log_sender: broadcast::Sender<String>,
 }
 
 impl Logger {
-    pub fn new(displayable: bool, module: String) -> Logger {
+    pub fn new(displayable: u8, module: String) -> Logger {
+        let (log_sender, _) = broadcast::channel(100);
+        let thread_log_sender = log_sender.clone();
+
+        thread::spawn(move || {
+            let rt = Runtime::new().unwrap();
+
+            rt.block_on(async move {
+                let ws_route = warp::path("ws")
+                    .and(warp::ws())
+                    .and(warp::any().map(move || thread_log_sender.clone()))
+                    .map(|ws: warp::ws::Ws, sender| {
+                        ws.on_upgrade(move |socket| handle_connection(socket, sender))
+                    });
+
+                println!("WebSocket server running on ws://127.0.0.1:3030");
+
+                // Run the warp server.
+                warp::serve(ws_route).run(([127, 0, 0, 1], 3030)).await;
+            });
+        });
+
         Logger {
             displayable,
             module,
+            log_sender,
         }
     }
 
-    pub fn set_displayable(&mut self, displayable: bool) {
+    pub fn set_displayable(&mut self, displayable: u8) {
         self.displayable = displayable;
     }
 
     pub fn log_debug(&self, message: &str) {
         let time = Local::now().format("%H:%M:%S:%3f").to_string();
-        if self.displayable {
-            println!(
-                "\x1b[32m[{}][DEBUG][{}]===={}\x1b[0m",
-                time, self.module, message
-            );
+        let formatted_message = format!("[{}][DEBUG][{}] {}", time, self.module, message);
+
+        if self.displayable & LogLevel::Debug as u8 != 0 {
+            println!("\x1b[32m{}\x1b[0m", formatted_message);
         }
+        
+        self.log_sender.send(formatted_message);
     }
 
     pub fn log_info(&self, message: &str) {
         let time = Local::now().format("%H:%M:%S:%3f").to_string();
-        if self.displayable {
-            println!(
-                "\x1b[34m[{}][INFO][{}]===={}\x1b[0m",
-                time, self.module, message
-            );
+        let formatted_message = format!("[{}][INFO][{}] {}", time, self.module, message);
+
+        if self.displayable & LogLevel::Info as u8 != 0 {
+            println!("\x1b[34m{}\x1b[0m", formatted_message);
         }
+        
+        self.log_sender.send(formatted_message);
     }
 
     pub fn log_warn(&self, message: &str) {
         let time = Local::now().format("%H:%M:%S:%3f").to_string();
-        if self.displayable {
-            println!(
-                "\x1b[33m[{}][WARN][{}]===={}\x1b[0m",
-                time, self.module, message
-            );
+        let formatted_message = format!("[{}][WARN][{}] {}", time, self.module, message);
+
+        if self.displayable & LogLevel::Warn as u8 != 0 {
+            println!("\x1b[33m{}\x1b[0m", formatted_message);
         }
+        
+        self.log_sender.send(formatted_message);
     }
 
     pub fn log_error(&self, message: &str) {
         let time = Local::now().format("%H:%M:%S:%3f").to_string();
-        if self.displayable {
-            println!(
-                "\x1b[31m[{}][ERR][{}]===={}\x1b[0m",
-                time, self.module, message
-            );
+        let formatted_message = format!("[{}][ERR][{}] {}", time, self.module, message);
+
+        if self.displayable & LogLevel::Error as u8 != 0 {
+            println!("\x1b[31m{}\x1b[0m", formatted_message);
+        }
+        
+        self.log_sender.send(formatted_message);
+    }
+}
+
+async fn handle_connection(ws: warp::ws::WebSocket, sender: broadcast::Sender<String>) {
+    let (mut ws_tx, _) = ws.split();
+    let mut rx = sender.subscribe();
+
+    while let Ok(message) = rx.recv().await {
+        if ws_tx.send(warp::ws::Message::text(message)).await.is_err() {
+            break;
         }
     }
 }
---
+++ src/main.rs
@@ -0,0 +1,18 @@
+mod logger;
+use logger::LogLevel;
+use std::{thread, time::Duration};
+
+fn main() {
+    
+    let logger = logger::Logger::new(LogLevel::Debug as u8 | LogLevel::Error as u8, "main".to_string());
+    loop {
+        //waiit for a second
+        logger.log_error("This is a error");
+        thread::sleep(Duration::from_millis(1000));
+        logger.log_debug("This is a debug message");
+        thread::sleep(Duration::from_millis(1000));
+        logger.log_info("This is a infoooo");
+        thread::sleep(Duration::from_millis(1000));
+    }
+}
+
